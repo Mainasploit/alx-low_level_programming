2be29f601bded81316b0cee61a704eefbb51f80b
mainasploit <mainasploit@gmail.com>
2023-09-07 15:17:28 -0700

    Updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-/**
-* main- Entry point
-* Description: This pinrts the size of various types
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-	printf("Size of char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
+
+int main() {
+    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+    printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+    printf("\n");
+
+    #ifdef __i386__
+    printf("Running on a 32-bit architecture.\n");
+    #elif __x86_64__
+    printf("Running on a 64-bit architecture.\n");
+    #else
+    printf("Architecture not recognized.\n");
+    #endif
+
+    return 0;
+}
 }
