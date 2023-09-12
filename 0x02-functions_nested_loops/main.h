22c5141a218b02673ea036b44a98c5dadd21424d
mainasploit <mainasploit@gmail.com>
2023-09-12 14:35:00 -0700

    Print to nth value

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,74 @@
+#ifndef __MAIN_
+#define _MAIN_
+
+/**
+ * _putchar -writes the character c to std
+ * @c: the parameter to compare
+ * Return: either 1 or 0
+ */
+int _putchar(char c);
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void);
+
+/**
+ * _islower - Print if argument is lowercase
+ * @c: argument to check
+ * Return: either 1 or 0
+ */
+int _islower(int c);
+
+/**
+ * _isalpha - Print if argument is alphabet
+ * @c: argument to check
+ * Return: Either 1 or 0
+ */
+int _isalpha(int c);
+
+/**
+ * print_sign - Print s the sign of a character
+ *
+ * @n: character to check
+ * Return:  either 1, 0 or -1
+ */
+int print_sign(int n);
+
+/**
+ * _abs - Print the absolute value
+ * Return : success
+ */
+int _abs(int);
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * Return 0 (Success)
+ */
+int print_last_digit(int);
+
+/**
+ * jack_buer - Printsevery minute
+ * Return: 0 (success)
+ */
+void jack_bauer(void);
+
+/**
+ * times_table- prints a multiplication table
+ */
+void times_table(void);
+/**
+ * add - Adds two numbers and returns result
+ *
+ */
+int add(int, int);
+
+/**
+ * print_to_98- print numbers to nth value
+ *
+ */
+void print_to_98(int n);
+
+#endif
